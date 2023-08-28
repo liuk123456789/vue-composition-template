fd22d5e36003f42401d31f00d74c206af3fa3379
kena <18329177975@163.com>
2023-08-28 17:36:39 +0800

    fix:修改layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,12 +1,11 @@
+<script lang="ts" setup>
+import { Layout, LayoutContent, LayoutFooter, LayoutHeader } from 'ant-design-vue'
+</script>
+
 <template>
-  <main
-    px-4 py-10
-    text="center gray-700 dark:gray-200"
-  >
-    <RouterView />
-    <TheFooter />
-    <div mx-auto mt-5 text-center text-sm opacity-50>
-      [Default Layout]
-    </div>
-  </main>
+  <Layout>
+    <LayoutHeader>Header</LayoutHeader>
+    <LayoutContent>Content</LayoutContent>
+    <LayoutFooter>Footer</LayoutFooter>
+  </Layout>
 </template>
--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -1,12 +1,13 @@
+<script lang="ts" setup>
+import { Image, Layout, LayoutContent, LayoutFooter, LayoutHeader } from 'ant-design-vue'
+</script>
+
 <template>
-  <main
-    px-4 py-10
-    text="center gray-700 dark:gray-200"
-  >
-    <RouterView />
-    <TheFooter />
-    <div mx-auto mt-5 text-center text-sm opacity-50>
-      [Home Layout]
-    </div>
-  </main>
+  <Layout>
+    <LayoutHeader class="flex justify-between flex-items-center">
+      <Image :width="50" :height="50" src="https://aliyuncdn.antdv.com/vue.png" />
+    </LayoutHeader>
+    <LayoutContent>Content</LayoutContent>
+    <LayoutFooter>Footer</LayoutFooter>
+  </Layout>
 </template>
